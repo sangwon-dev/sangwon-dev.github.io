9b596726f7398037fd192645f7833c93ca6f53fe
Sangwon <sangwon2618@gamil.com>
2023-04-15 14:14:07 +0900

    ADD project

---
+++ calc/script.js
@@ -0,0 +1,80 @@
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+document
+  .getElementById('initialAmount')
+  .addEventListener('blur', function (event) {
+    const value = event.target.value.replace(/[^0-9]/g, '');
+    event.target.value = numberWithCommas(value);
+  });
+
+document
+  .getElementById('calculator')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const initialAmount = parseFloat(
+      document.getElementById('initialAmount').value.replace(/,/g, '')
+    );
+    const interestRate =
+      parseFloat(document.getElementById('interestRate').value) / 100;
+    const period = parseInt(document.getElementById('period').value);
+    const periodUnit = document.getElementById('periodUnit').value;
+
+    let intervals = period;
+    let intervalUnit;
+    if (periodUnit === 'months') {
+      intervals = period;
+      intervalUnit = 'month';
+    } else if (periodUnit === 'years') {
+      intervals = period;
+      intervalUnit = 'year';
+    } else {
+      intervalUnit = 'day';
+    }
+
+    const resultTable = document.getElementById('resultTable');
+    const resultTableBody = document.getElementById('resultTableBody');
+    resultTableBody.innerHTML = '';
+    resultTable.style.display = 'table';
+
+    let totalAmount = initialAmount;
+    let currentDate = new Date();
+    currentDate.setHours(9, 0, 0, 0);
+
+    for (let i = 0; i < intervals; i++) {
+      const interest = totalAmount * interestRate;
+      totalAmount += interest;
+
+      const tr = document.createElement('tr');
+      const tdDate = document.createElement('td');
+      tdDate.textContent = currentDate.toISOString().slice(0, 10);
+      tr.appendChild(tdDate);
+
+      const tdInterest = document.createElement('td');
+      tdInterest.textContent = numberWithCommas(interest.toFixed(0)) + '원';
+      tr.appendChild(tdInterest);
+
+      const tdTotalAmount = document.createElement('td');
+      tdTotalAmount.textContent =
+        numberWithCommas(totalAmount.toFixed(0)) + '원';
+      tr.appendChild(tdTotalAmount);
+
+      const tdInterestRate = document.createElement('td');
+      tdInterestRate.textContent =
+        numberWithCommas(((totalAmount / initialAmount - 1) * 100).toFixed(2)) +
+        '%';
+      tr.appendChild(tdInterestRate);
+
+      resultTableBody.appendChild(tr);
+
+      if (intervalUnit === 'month') {
+        currentDate.setMonth(currentDate.getMonth() + 1);
+      } else if (intervalUnit === 'year') {
+        currentDate.setFullYear(currentDate.getFullYear() + 1);
+      } else {
+        currentDate.setDate(currentDate.getDate() + 1);
+      }
+    }
+  });
